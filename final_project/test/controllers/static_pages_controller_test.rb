22eec1df86541e507aa311538eb8cc1c1f033a4c
andrew-k512 <andrew.fiveonetwo@gmail.com>
2017-08-28 13:40:48 -0400

    synch html, css, and images to rails app. configured routes and nav links

--- final_project/test/controllers/static_pages_controller_test.rb
+++ final_project/test/controllers/static_pages_controller_test.rb
@@ -11,4 +11,22 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  def setup
+    @base_title = "Noble Dating App"
+  end
+
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+    assert_select "title", "Home | #{@base_title}"
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | #{@base_title}"
+  end
+end
+
+
 end
