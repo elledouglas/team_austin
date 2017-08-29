ffec24a34ad3785c1dfff3e21af1f39044d8f8a3
andrew-k512 <andrew.fiveonetwo@gmail.com>
2017-08-28 22:55:49 -0400

    tweak timeline.txt, fix a few tests (some still not working)

--- final_project/test/controllers/static_pages_controller_test.rb
+++ final_project/test/controllers/static_pages_controller_test.rb
@@ -1,15 +1,15 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
-  end
-
-  test "should get about" do
-    get static_pages_about_url
-    assert_response :success
-  end
+  # test "should get home" do
+  #   get static_pages_home_url
+  #   assert_response :success
+  # end
+  #
+  # test "should get about" do
+  #   get static_pages_about_url
+  #   assert_response :success
+  # end
 
   def setup
     @base_title = "Noble Dating App"
@@ -26,7 +26,4 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
-end
-
-
 end
